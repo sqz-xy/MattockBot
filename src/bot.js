bb50efb79e76333a95490f29a7fef116368085ea
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2021-10-11 18:09:52 +0100

    Bot can now be brought online

--- src/bot.js
+++ src/bot.js
@@ -1,2 +1,7 @@
+// Bot token saved as environmental variable 
 require('dotenv').config();
 
+const { Client, Intents } = require('discord.js'); 
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
