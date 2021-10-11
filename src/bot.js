7c6c47b2444f88d6c23019d732fff0f036247bb1
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2021-10-11 18:14:14 +0100

    Added client login message

--- src/bot.js
+++ src/bot.js
@@ -4,4 +4,8 @@ require('dotenv').config();
 const { Client, Intents } = require('discord.js'); 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in!`);
+})
+
 client.login(process.env.DISCORDJS_BOT_TOKEN);
