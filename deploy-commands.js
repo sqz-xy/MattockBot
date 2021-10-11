1dd93fac06f9b7936191e232ebfa40fc7316e8e0
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2021-10-11 20:16:35 +0100

    Modified file pathing

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,6 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { token, guildId, clientId } = require('./src/config.json');
+const { token, guildId, clientId } = require('./config.json');
 const fs = require('fs');
 
 const commands = [];
