2faa7c19cb3b4fd72777bea0942110142e310ae6
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2021-10-11 19:51:41 +0100

    completely restructured files and commands are now stored in a folder

---
+++ index.js
@@ -0,0 +1,35 @@
+// Token saved in config.json
+const config = require('./config.json');
+const fs = require('fs');
+const { Client, Intents, Collection } = require('discord.js'); 
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+    console.log(`${client.user.tag} has logged in!`);
+})
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.login(config.token);
+
